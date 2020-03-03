05de7b98a3b4e72c93d9e542597fbf35b037360d
muckystack <muckystack@gmail.com>
2020-03-03 14:56:16 -0600

    Select

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
+import 'package:qrreaderappfh/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 
@@ -42,6 +43,43 @@ class DBProvider {
       }
     );
 
-  }  
+  }
+
+
+  // CREAR registros
+  nuevoScanRaw(ScanModel nuevoScan) async {
+
+    final db = await database;
+
+    final res = await db.rawInsert(
+      "INSERT Into Scans (id, tipo, valor) "
+      "VALUES(${nuevoScan.id}, '${nuevoScan.tipo}', '${nuevoScan.valor}')"
+    );
+
+    return res;
+
+  }
+
+  nuevoScan(ScanModel nuevoScan) async {
+
+    final db = await database;
+
+    final res = await db.insert('Scans', nuevoScan.toJson());
+
+    return res;
+
+  }
+
+
+  // SELECT - Obtener información
+  Future<ScanModel> getScanById(int id) async {
+
+    final db = await database;
+
+    final res = await db.query('Scans', where: 'id = ?', whereArgs: [id]);
+
+    return res.isNotEmpty ? ScanModel.fromJson(res.first) : null;
+
+  }
 
 }
