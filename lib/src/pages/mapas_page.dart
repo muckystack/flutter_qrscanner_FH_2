d897a8c8a53f3e089ee77e846194cba8eef6b92e
muckystack <muckystack@gmail.com>
2020-03-04 11:46:27 -0600

    Show data in page

--- lib/src/pages/mapas_page.dart
+++ lib/src/pages/mapas_page.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderappfh/src/providers/db_provider.dart';
 
 class MapasPage extends StatelessWidget {
   const MapasPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('Mapas Page'),),
+    return FutureBuilder(
+      future: DBProvider.db.getTodosScans(),
+      builder: (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
+        
+        if(!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        final scans = snapshot.data;
+
+        if(scans.length == 0) {
+          return Center(child: Text('No hay datos'));
+        }
+
+        return ListView.builder(
+          itemCount: scans.length,
+          itemBuilder: (context, i) => ListTile(
+            leading: Icon(Icons.cloud_queue, color: Theme.of(context).primaryColor),
+            title: Text(scans[i].valor),
+            trailing: Icon(Icons.keyboard_arrow_right),
+          ),
+        );
+      
+      },
     );
   }
 }
