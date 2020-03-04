b2de89cd8db231c520d6bdcc2cea829ff48a5789
muckystack <muckystack@gmail.com>
2020-03-04 00:09:58 -0600

    SQL

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
 import 'package:qrreaderappfh/src/models/scan_model.dart';
+export 'package:qrreaderappfh/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 
@@ -66,6 +67,8 @@ class DBProvider {
 
     final res = await db.insert('Scans', nuevoScan.toJson());
 
+    // print(res);
+
     return res;
 
   }
@@ -79,6 +82,74 @@ class DBProvider {
     final res = await db.query('Scans', where: 'id = ?', whereArgs: [id]);
 
     return res.isNotEmpty ? ScanModel.fromJson(res.first) : null;
+
+  }
+  
+  
+  // SELECT - Obtener todos los registros
+  Future<List<ScanModel>> getAllScans() async {
+
+    final db = await database;
+
+    final res = await db.query('Scans');
+
+    List<ScanModel> list = res.isNotEmpty
+                          ? res.map((c) => ScanModel.fromJson( c)).toList()
+                          : [];
+
+    return list;
+
+  }
+  
+  
+  // SELECT - Obtener todos los registros por tipo
+  Future<List<ScanModel>> getAllScansByTipo(String tipo) async {
+
+    final db = await database;
+
+    final res = await db.rawQuery("SELECT * FROM Scans WHERE tipo=$tipo");
+
+    List<ScanModel> list = res.isNotEmpty
+                          ? res.map((c) => ScanModel.fromJson( c)).toList()
+                          : [];
+
+    return list;
+
+  }
+  
+  
+  // UPDATE
+  Future<int> updateScan(ScanModel nuevoScan) async {
+
+    final db = await database;
+
+    final res = await db.update('Scans', nuevoScan.toJson(), where: 'id = ?', whereArgs: [nuevoScan.id]);
+
+    return res;
+
+  }
+  
+  
+  // DELETE
+  Future<int> deleteScan(int id) async {
+
+    final db = await database;
+
+    final res = await db.delete('Scans', where: 'id = ?', whereArgs: [id]);
+
+    return res;
+
+  }
+  
+  
+  // DELETE ALL
+  Future<int> deleteAllScans() async {
+
+    final db = await database;
+
+    final res = await db.rawDelete('DELETE FROM Scans');
+
+    return res;
 
   }
 
