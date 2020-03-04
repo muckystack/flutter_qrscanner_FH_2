cc42faa853af043d3271d306ab698510b820ef01
muckystack <muckystack@gmail.com>
2020-03-04 17:18:29 -0600

    Funciona el mapa

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:qrreaderappfh/src/models/scan_model.dart';
 
 class MapaPage extends StatelessWidget {
@@ -19,9 +20,33 @@ class MapaPage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text(scan.valor),
-      ),
+      body: _crearFlutterMap(scan),
     );
+  }
+
+  Widget _crearFlutterMap(ScanModel scan) {
+    return FlutterMap(
+      options: MapOptions(
+        center: scan.getLatLng(),
+        zoom: 10
+      ),
+      layers: [
+        _crearMapa(),
+      ],
+    );
+  }
+
+
+  _crearMapa() {
+
+    return TileLayerOptions(
+      urlTemplate: 'https://api.mapbox.com/v4/'
+      '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+      additionalOptions: {
+        'accessToken': 'pk.eyJ1IjoibXVja3lzdGFjayIsImEiOiJjazU0eGhidHcwMHZ3M2xzN2w0eXdsdmJkIn0.MvGNRnHmgDF5syHT5Wra7w',
+        'id': 'mapbox.streets'
+      }
+    );
+
   }
 }
